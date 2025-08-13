3f4c3600fbec9c37017478f13b2f28259c6e30ad
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-08-13 17:19:37 -0300

    add login page and global styles

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './styles.css';
+import logoImage from '../../assets/logo.svg';
+import padlock from '../../assets/padlock.png';
+
+export default function Login(){
+    return(
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Logo Login"/>
+                <form>
+                    <h1>Access your account</h1>
+                    <input placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit" className="button">Login</button>
+                </form>
+            </section>
+
+            <img src={padlock} alt="Login"/>
+        </div>
+    );
+}
