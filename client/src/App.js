19e2d1df42bde874dfabc9573ec3453e1086f845
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-08-13 18:00:17 -0300

    refactor routing structure and update dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import './global.css';
-import Login from "./pages/Login";
+import AppRoutes from "./routes";
 
 export default function App() {
   return (
-      <Login />
+      <AppRoutes/>
   );
 }
 
---
+++ client/src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Book from "./pages/Book";
+
+export default function AppRoutes() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />}/>
+                <Route path="/book" element={<Book />}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
